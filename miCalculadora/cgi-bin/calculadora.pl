a7a20e4d71bd27ceb5f8122f45ef81942ac38174
Fernando Guevara <fluqueg@unsa.edu.pe>
2024-10-26 21:45:14 -0500

    Capturo cualquier error con @, si no pasa nada, entonces imprimira en la pagina web el resultado de la operacion

--- miCalculadora/cgi-bin/calculadora.pl
+++ miCalculadora/cgi-bin/calculadora.pl
@@ -9,6 +9,11 @@ print $query->header('text/html');
 my $expression = $query->param('expresion');
 if ($expression =~ /^[\d\+\-\*\/\s]+$/) {
     my $result = eval($expression);
+    if ($@) {
+        print "<h1>Error: Solo admite sumas, restas, multiplicacion y division (por cero no)</h1>";
+    } else {
+        print "<h1>Resultado: $result</h1>";
+    }
 }else {
     print "<h1>Invalido, intente no usar espacios.</h1>";
 }
