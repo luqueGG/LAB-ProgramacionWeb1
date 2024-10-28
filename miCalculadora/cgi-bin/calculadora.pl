b805b6caa9ee77cd95ec5836bf1dd7de2ff9af0e
Fernando Guevara <fluqueg@unsa.edu.pe>
2024-10-27 19:13:29 -0500

    Laboratorio05 - Luque_Guevara - Pweb1

--- miCalculadora/cgi-bin/calculadora.pl
+++ miCalculadora/cgi-bin/calculadora.pl
@@ -5,15 +5,77 @@ use CGI;
 
 my $query = CGI->new;
 print $query->header('text/html');
+my $expresion = $query->param('expresion');
 
-my $expression = $query->param('expresion');
-if ($expression =~ /^[\d\+\-\*\/\s]+$/) {
-    my $result = eval($expression);
-    if ($@) {
-        print "<h1>Error: Solo admite sumas, restas, multiplicacion y division (por cero no)</h1>";
-    } else {
-        print "<h1>Resultado: $result</h1>";
+if ($expresion =~ /^[\d\+\-\*\/\(\)\s]+$/) {
+    # Quitar espacios
+    $expresion =~ s/\s+//g;
+
+    my @posOrden = cambiarPosFija($expresion);
+    my $resultado = evaluarPosOrden(@posOrden);
+
+    print "<h1>Resultado: $resultado</h1>";
+} else {
+    print "<h1>Formato Denegado.</h1>";
+}
+
+# Funcion para convertir una expresion infija a posfija
+sub cambiarPosFija {
+    my ($expr) = @_; #Parametro de la funcion
+    my @salidaExpresion;
+    my @operadores;
+    my %precedence = ('+' => 1, '-' => 1, '*' => 2, '/' => 2);
+
+    my @pares = split /(\d+|\+|\-|\*|\/|\(|\))/, $expr;
+    @pares = grep { $_ ne '' } @pares; # Elimina pares vacios
+
+    foreach my $par (@pares) {
+        if ($par =~ /^\d+$/) {
+            push @salidaExpresion, $par;
+        } elsif ($par =~ /[\+\-\*\/]/) {
+            while (@operadores && $operadores[-1] ne '(' && $precedence{$operadores[-1]} >= $precedence{$par}) {
+                push @salidaExpresion, pop @operadores;
+            }
+            push @operadores, $par;
+        } elsif ($par eq '(') {
+            push @operadores, $par;
+        } elsif ($par eq ')') {
+            while (@operadores && $operadores[-1] ne '(') {
+                push @salidaExpresion, pop @operadores;
+            }
+            pop @operadores; # Remover '('
+        }
     }
-}else {
-    print "<h1>Invalido, intente no usar espacios.</h1>";
+    # Agregar operadores restantes
+    push @salidaExpresion, reverse @operadores;
+    return @salidaExpresion;
+}
+
+# Funcion para evaluar la expresion posfija
+sub evaluarPosOrden {
+    my @exprPos = @_;
+    my @resultado;
+
+    foreach my $par (@exprPos) {
+        if ($par =~ /^\d+$/) {
+            push @resultado, $par;
+        } else {
+            my $b = pop @resultado;
+            my $a = pop @resultado;
+
+            if ($par eq '+') {
+                push @resultado, $a + $b;
+            } elsif ($par eq '-') {
+                push @resultado, $a - $b;
+            } elsif ($par eq '*') {
+                push @resultado, $a * $b;
+            } elsif ($par eq '/') {
+                if ($b == 0) {
+                    die "Error: Division por cero";
+                }
+                push @resultado, $a / $b;
+            }
+        }
+    }
+    return pop @resultado;
 }
